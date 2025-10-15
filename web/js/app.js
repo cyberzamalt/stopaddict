dffa4b702609ffcfdcfe6425b20085e52b1f6c74
cyberzamalt <abriounicolas@gmail.com>
2025-10-15 17:50:04 +0200

    Update app.js (#46)

--- web/js/app.js
+++ web/js/app.js
@@ -2,10 +2,12 @@ import { initCounters }     from "./counters.js";
 import { initSettings }     from "./settings.js";
 import { initImportExport } from "./export.js";
 import { initStatsHeader }  from "./stats.js";
+import { initChart }        from "./chart.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   initCounters();
   initSettings();
   initImportExport();
   initStatsHeader();
+  initChart();
 });
