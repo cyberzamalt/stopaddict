b75b715df0e0d2ec6b535ce0ac6fc8123c782649
cyberzamalt <abriounicolas@gmail.com>
2025-10-16 13:47:12 +0200

    Replace initChart and initAgenda with initCharts (#61)

--- web/js/app.js
+++ web/js/app.js
@@ -2,16 +2,14 @@ import { initCounters }     from "./counters.js";
 import { initSettings }     from "./settings.js";
 import { initImportExport } from "./export.js";
 import { initStatsHeader }  from "./stats.js";
-import { initChart }        from "./chart.js";
-import { initAgenda }       from "./agenda.js";
 import { initLimits }       from "./limits.js";
+import { initCharts }       from "./charts.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   initCounters();
   initSettings();
   initImportExport();
   initStatsHeader();
-  initChart();
-  initAgenda();
   initLimits();
+  initCharts();
 });
